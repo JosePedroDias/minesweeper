e40f7f04b1bfb8f371c8340fb58180ed1bb94564
José Pedro Dias <jose.dias@arkadium.com>
2024-04-07 22:30:57 +0100

    sasd

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     println!("Hello, world!");
     let b = Board::new((16, 16), 32);
     println!("{:#?}", b);
+    println!("{}", b);
 }
--- src/state.rs
+++ src/state.rs
@@ -1,20 +1,21 @@
 use std::collections::{HashMap, HashSet};
 use rand::Rng;
+use std::fmt;
 
 #[derive(Debug)]
 pub struct Cell {
-    pub uncovered: bool, // left mouse button action
+    pub is_uncovered: bool, // left mouse button action
     pub is_flagged: bool, // right mouse button action
-    pub has_mine: bool, // hidden state
+    pub is_mine: bool, // hidden state
     pub hint_number: u8, // hint number to display in case of uncover/flood
 }
 
 impl Cell {
-    pub fn new(has_mine: bool) -> Self {
+    pub fn new(is_mine: bool) -> Self {
         Cell {
-            uncovered: false,
+            is_uncovered: true,
             is_flagged: false,
-            has_mine,
+            is_mine,
             hint_number: 0,
         }
     }
@@ -58,6 +59,89 @@ impl Board {
                 b.cells.insert(pos, Cell::new(mine_positions.contains(&pos)));
             }
         }
+        
+        for y in 0..size.1 {
+            for x in 0..size.0 {
+                let pos = (x, y);
+                let count = b.count_neighbors(&pos);
+                let cell = b.get_cell_mut(&pos);
+                if let Some(cell2) = cell {
+                    if cell2.is_mine {
+                        continue;
+                    }
+                    cell2.hint_number = count;
+                }
+            }
+        }
+        
         b
+    }
+    pub fn get_cell(self: &Self, pos: &(u8, u8)) -> Option<&Cell> {
+        self.cells.get(pos)
+    }
+    pub fn get_cell_mut(self: &mut Self, pos: &(u8, u8)) -> Option<&mut Cell> {
+        self.cells.get_mut(pos)
+    }
+    fn count_neighbors(self: &Self, pos: &(u8, u8)) -> u8 {
+        let mut count: u8 = 0;
+        for y in -1..=1i8 {
+            for x in -1..=1i8 {
+                if x == 0 && y == 0 {
+                    continue;
+                }
+                let xx = (x + (pos.0 as i8)) as u8;
+                let yy = (y + (pos.1 as i8)) as u8;
+                let cell = self.get_cell(&(xx, yy));
+                if let Some(cell2) = cell {
+                    if cell2.is_mine {
+                        count += 1;
+                    }
+                }
+            }
+        }
+        count
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut st = String::new();
+        for y in 0..self.size.1 {
+            for x in 0..self.size.0 {
+                let pos = (x, y);
+                let cell = self.get_cell(&pos).unwrap();
+                if cell.is_uncovered {
+                    if cell.is_mine {
+                        st.push('M');
+                    } else if cell.hint_number == 1 {
+                        st.push('1');
+                    } else if cell.hint_number == 2 {
+                        st.push('2');
+                    } else if cell.hint_number == 3 {
+                        st.push('3');
+                    } else if cell.hint_number == 4 {
+                        st.push('4');
+                    } else if cell.hint_number == 5 {
+                        st.push('5');
+                    } else if cell.hint_number == 6 {
+                        st.push('6');
+                    } else if cell.hint_number == 7 {
+                        st.push('7');
+                    } else if cell.hint_number == 8 {
+                        st.push('8');
+                    } else {
+                        st.push('_');
+                    }
+                } else {
+                    if cell.is_flagged {
+                        st.push('F');
+                    } else {
+                        st.push('?');
+                    }
+                }
+            }
+            st.push('\n');
+        }
+        write!(f, "{}", st)
     }
 }

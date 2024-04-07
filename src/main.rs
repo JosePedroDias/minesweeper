d1cb5ef9b2c5e347dc016768012cb53f53778537
José Pedro Dias <jose.dias@arkadium.com>
2024-04-07 21:18:02 +0100

    asd

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ use state::*;
 fn main() {
     println!("Hello, world!");
     let b = Board::new((16, 16), 32);
-    println!("{:?}", b);
+    println!("{:#?}", b);
 }
--- src/state.rs
+++ src/state.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use rand::Rng;
 
 #[derive(Debug)]
 pub struct Cell {
@@ -22,17 +23,41 @@ impl Cell {
 #[derive(Debug)]
 pub struct Board {
     //start_time:
+    pub size: (u8, u8),
+    pub game_ended: bool,
+    pub has_won: bool,
     cells: HashMap<(u8, u8), Cell>,
-    game_ended: bool,
-    has_won: bool,
 }
 
 impl Board {
     pub fn new(size: (u8, u8), num_mines: u16) -> Self {
-        Board {
+        let mut b = Board {
+            size,
             cells: HashMap::new(),
             game_ended: false,
             has_won: false,
+        };
+        
+        let mut rng = rand::thread_rng();
+        let mut mine_positions: HashSet<(u8, u8)> = HashSet::new();
+        for _ in 0..num_mines {
+            loop {
+                let x = rng.gen_range(0..size.0);
+                let y = rng.gen_range(0..size.1);
+                let pos = (x, y);
+                if !mine_positions.contains(&pos) {
+                    mine_positions.insert(pos);
+                    break;
+                }
+            }
         }
+        
+        for y in 0..size.1 {
+            for x in 0..size.0 {
+                let pos = (x, y);
+                b.cells.insert(pos, Cell::new(mine_positions.contains(&pos)));
+            }
+        }
+        b
     }
 }
